30183ffcc6b36af4efa4025ba95c8f42c36ae344
Zhong Li <neozl24@hotmail.com>
2024-10-27 02:30:28 +0800

    make modal component code more concise

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,5 +1,3 @@
-import { useRef } from "react"
-
 interface ModalProps {
   className?: string
   title: string
@@ -14,35 +12,12 @@ export const Modal = ({
   children,
 }: ModalProps) => {
   return (
-    <Mask onClick={onClose}>
-      <div className={`px-10 py-16 bg-white border border-neutral-600 ${className}`}>
+    <div className='fixed w-full h-full flex justify-center items-center z-[1000]'>
+      <div className='absolute top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,0.4)]' onClick={onClose} />
+      <div className={`px-10 py-16 bg-white border border-neutral-600 z-[1] ${className}`}>
         <h4 className='mb-12 text-center text-2xl italic text-neutral-500'>{title}</h4>
         {children}
       </div>
-    </Mask>
-  )
-}
-
-const Mask = ({
-  children,
-  onClick,
-}: {
-  children: React.ReactNode
-  onClick?: () => void
-}) => {
-  const containerRef = useRef<HTMLDivElement>(null)
-
-  return (
-    <div
-      className='fixed w-full h-full flex justify-center items-center bg-[rgba(0,0,0,0.4)]'
-      onClick={(e) => {
-        if (e.target === containerRef.current) {
-          onClick?.()
-        }        
-      }}
-      ref={containerRef}
-    >
-      {children}
     </div>
   )
 }
